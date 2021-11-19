0baf0b32053d439f5314cd6c47bc4c1492933771
sedov1 <viking182@mail.ru>
2021-11-19 21:58:33 +0300

    дороботка

--- src/HomeWorkApp.java
+++ src/HomeWorkApp.java
@@ -7,15 +7,14 @@ public class HomeWorkApp {//создание класса
 
         printThreeWords();
         checkSumSign();// название метода
-        printColor();
-
+        printColor(10);
+        compareNumbers();
     }
 
     public static void printThreeWords() { // создание метода
         System.out.println("Orange"); // вывод в консольной панели
         System.out.println("Banana");
         System.out.println("Apple");
-
 
 
     }
@@ -25,19 +24,34 @@ public class HomeWorkApp {//создание класса
         int b = -20; // переменная b
         int c = 0; // переменная с
         System.out.println(c); // вывод в консоль переменную с
-        if (a<c&&b>c) {
+        if (a < c && b > c) {
             System.out.println("Сумма положительная");
         } else {
             System.out.println("Сумма отрицательная");
         }
 
-        }
-public static void printColor(){
-        int value=20;
-        if(value>0){
+    }
+
+    public static void printColor(int value) {
+        if (value <= 0) {
             System.out.println("Красный");
         }
+        if (value > 0 && value < 100) {
+            System.out.println("Желтый");
+        }
+        if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
 
+    public static void compareNumbers() {
+        int a = 6;
+        int b = 9;
+if(a>=b) {
+    System.out.println("a>=b");
+} else  {
+    System.out.println("a<b");
+}
     }
 }
 

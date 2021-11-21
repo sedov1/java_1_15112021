1a037f006a7fc1732c38c31c5b898dab458215af
sedov1 <viking182@mail.ru>
2021-11-21 20:14:03 +0300

    дом задание доl

--- src/HomeWorkApp2.java
+++ src/HomeWorkApp2.java
@@ -1,7 +1,12 @@
 public class HomeWorkApp2 {
 
+
     public static void main(String[] args) {
-        int x = 10 ;
+         methodA();
+         methodD();
+
+
+        int x = 10;
         int y = 5;
 
         if ((x + y) > 10 && (x + y) < 20) {
@@ -12,7 +17,32 @@ public class HomeWorkApp2 {
 
     }
 
+    public static void methodA() {
+        int a = -4;
+        if (a < 0) {
+            System.out.println("Число отрицательное.");
+        } else {
+            System.out.println("Число положительное.");
+
+        }
+    }
+
+    public static boolean methodD() {
+        int a = 6;
+        if (a < 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+       }
 
 
 
-}
+
+
+
+
+

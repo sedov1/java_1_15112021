b96c194b902479a48812b2dbb777aa215a08efb8
sedov1 <viking182@mail.ru>
2021-11-24 22:11:29 +0300

    урок 3 на прверку

--- src/HomrWorkApp3.java
+++ src/HomrWorkApp3.java
@@ -14,11 +14,11 @@ public class HomrWorkApp3 {
         for (int i = 0; i < arr.length; i++) ;
         {
             if (arr[i] == 1) {
-
+             arr[i]=1;
             } else {
                 arr[i] = 0;
             }
-            System.out.println(Arrays.toString(arr));
+            System.out.println(arr[i]);
         }
 
     }
